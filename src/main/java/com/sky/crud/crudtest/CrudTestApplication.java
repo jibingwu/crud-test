3a4367f586e056b48f1c2d4a7afb3204f582470b
jsonwu <1670544286@qq.com>
2019-12-27 13:15:14 +0800

    Initial commit

---
+++ src/main/java/com/sky/crud/crudtest/CrudTestApplication.java
@@ -0,0 +1,15 @@
+package com.sky.crud.crudtest;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages="com.sky.crud.crudtest.mapper")
+public class CrudTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sky/crud/crudtest/controller/MemberController.java
@@ -0,0 +1,28 @@
+package com.sky.crud.crudtest.controller;
+
+
+import com.sky.crud.crudtest.mapper.TMemberMapper;
+import com.sky.crud.crudtest.pojo.TMember;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MemberController {
+
+    @Autowired
+    TMemberMapper memberMapper;
+
+    @RequestMapping("/test")
+    public TMember queryMemberId(@RequestParam(value = "id", defaultValue = "1") Integer id) {
+
+        TMember tMember = memberMapper.selectByPrimaryKey(id);
+
+        return tMember;
+    }
+
+
+
+
+}
